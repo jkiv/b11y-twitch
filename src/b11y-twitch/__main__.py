0695e091d8d4439c5b6293fbafac8f9e2d9be9b9
Jon Kivinen <226881+jkiv@users.noreply.github.com>
2021-06-26 22:08:25 -0400

    Full loop echo using MQTT.

--- src/b11y-twitch/__main__.py
+++ src/b11y-twitch/__main__.py
@@ -11,6 +11,8 @@ class Bot(commands.Bot):
         self._write_queue = write_queue
         self._config = config
 
+        self._mods_only = True
+
         super().__init__(
             irc_token=config['irc_token'],
             client_id=config['client_id'],
@@ -29,12 +31,16 @@ class Bot(commands.Bot):
     async def event_message(self, message):
         print(f'[{message.timestamp}] {message.author.name}: {message.content}', flush=True)
 
-        if message.author.is_mod:
+        if message.author.is_mod or not self._mods_only:
             await self.handle_commands(message)
 
     @commands.command(name='ping')
     async def ping(self, ctx):
         await ctx.send(f'pong @{ctx.author.name}!')
+
+    @commands.command(name='togglemod')
+    async def toggle_mod(self, ctx):
+        self._mods_only = not self._mods_only
 
     @commands.command(name='mqtt')
     async def mqtt_echo(self, ctx):
@@ -44,6 +50,7 @@ class Bot(commands.Bot):
         try:
             _, topic, payload = ctx.message.content.split(' ', 2)
         except:
+            print(f'Malformed !mqtt command: {ctx.message.content}', flush=True)
             return # Malformed command
 
         print(f'mqtt_echo: Enqueueing \'[{timestamp}] {user}: {topic=}, {payload=}\'', flush=True)
@@ -55,7 +62,7 @@ async def _handle_twitch_to_mqtt_forever(mqtt_client, read_queue):
     while loop.is_running():
         # Publish messages from read_queue
         topic, message = await read_queue.get()
-        print(f'twitch->mqtt: {topic=}, {message=}', flush=True)
+        print(f'Publishing MQTT message: {topic=}, {message=}', flush=True)
         mqtt_client.publish(topic, message)
         read_queue.task_done()
 
@@ -64,58 +71,60 @@ def on_mqtt_connect_cb(client, userdata, flags, rc):
     client.subscribe('b11y/dev/*')
 
 def on_mqtt_message_cb(client, userdata, msg, write_queue):
-    print(f'{msg.topic}: {msg.payload}', flush=True)
+    print(f'MQTT message received: {msg.topic}: {msg.payload}', flush=True)
     write_queue.put_nowait((msg.topic, msg.payload,))
 
 async def _handle_mqtt_to_twitch_forever(twitch_client, read_queue):
     loop = asyncio.get_running_loop()
 
     while loop.is_running():
-        topic, message = await read_queue.get()
+        topic, payload = await read_queue.get()
+        print(f'Pulled MQTT payload off internal queue: {topic=}, {payload=}', flush=True)
 
-        # TODO how to send a message via twitch_client?
-        print(f'Received MQTT message: {topic=}, {message=}', flush=True)
-        print('  - TODO handle properly')
+        channel = twitch_client.get_channel('jkiv')
+        await channel.send(f'<mqtt> {topic=}, {payload=}')
 
         read_queue.task_done()
     
 async def _mqtt_loop(mqtt_client):
+
+    def _blocking_loop(mqtt_client):
+        return mqtt_client.loop()
+
     loop = asyncio.get_running_loop()
-    # run_in_executor?
-    rc = 0
-    while rc == 0:
-        rc = mqtt_client.loop()
+
+    while loop.is_running():
+        await loop.run_in_executor(None, _blocking_loop, mqtt_client)
 
 async def main(config):
 
+    loop = asyncio.get_running_loop()
+
+    # Set up internal message queues
     twitch_to_mqtt_queue = asyncio.Queue()
     mqtt_to_twitch_queue = asyncio.Queue()
 
-    loop = asyncio.get_running_loop()
+    # Set up TwitchIO client
     twitch_client = Bot(mqtt_to_twitch_queue, twitch_to_mqtt_queue, loop=loop, **config)
 
-    # Set up MQTT
+    # Set up MQTT client
+    # TODO config['mqtt_username'], config['mqtt_password'], ...
     mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_mqtt_connect_cb
     mqtt_client.on_message = lambda client, userdata, message: on_mqtt_message_cb(client, userdata, message, mqtt_to_twitch_queue)
-
     mqtt_client.username_pw_set(username='mqtt_anonymous', password='mqtt_anonymous')
-
-    mqtt_client.connect_async("localhost", 1883, 60)
-    mqtt_client.loop_start()
+    mqtt_client.connect('localhost', 1883, 60)
 
     try:
         await asyncio.gather(
             _handle_twitch_to_mqtt_forever(mqtt_client, twitch_to_mqtt_queue),
             _handle_mqtt_to_twitch_forever(twitch_client, mqtt_to_twitch_queue),
+            _mqtt_loop(mqtt_client),
             twitch_client.start()
         )
     except Exception as e:
-        #_log.error(f'Fatal error: {e!r}')
-        #_log.error(f'Quitting...')
+        print(f'Exception: {e!r}', flush=True)
         loop.stop()
-    
-    mqtt_client.loop_stop()
 
 if __name__ == '__main__':
 
